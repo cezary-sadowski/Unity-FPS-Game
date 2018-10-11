fe50da40c500577ba0d0200471ed59607e418461
Cezary <cezary@MacBook-Air-Cezary.local>
2018-10-11 18:50:46 +0200

    Raycast shooting without animation

--- FPS/Assets/Scripts/Weapon/Weapon.cs
+++ FPS/Assets/Scripts/Weapon/Weapon.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
 
+    public float range = 100f;
+
+    public int bulletsPerMag = 30;
+    public int bulletsLeft;
+
+    public Transform gunEnd; //the place on gun from which the missiles fly out :) muzzle!
+
+    public float fireRate = 0.25f; //250 miliseconds between shots. 
+
+    float fireTimer;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+
+        if (fireTimer < fireRate)
+            fireTimer += Time.deltaTime; //Add into time counter.
 	}
+
+    private void Fire()
+    {
+        if (fireTimer < fireRate) return;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(gunEnd.position, gunEnd.transform.forward, out hit, range)) //Raycast shooting convention 
+        {
+            Debug.Log(hit.transform.name + " found!");
+        }
+
+        fireTimer = 0.0f; //Reset fire timer before fire.
+    }
 }
